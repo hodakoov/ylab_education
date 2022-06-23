a77c88c8ccb6d86161bec362a41ef140822053cc
Valery <teeror93@gmail.com>
2022-06-24 01:25:45 +0400

    Lecture #1 Task #3 Написать метод zeros, который принимает на вход целое число (integer) и возвращает количество конечных нулей в факториале (N! = 1 * 2 * 3 * ... * N) заданного числа. Корректировка по оформлению предыдущих методов

--- domain_name.py
+++ domain_name.py
@@ -1,4 +1,12 @@
-def domain_name(url):
+"""
+Написать метод domain_name, который вернет домен из url адреса:
+url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+url = "https://www.cnet.com"                -> domain name = "cnet"
+"""
+
+
+def domain_name(url: str):
     if 'www' in url:
         url = url[(url.find('.')) + 1:]
         return url[:(url.rfind('.'))]
@@ -7,3 +15,13 @@ def domain_name(url):
         while '.' in url:
             url = url[:url.rfind('.')]
         return url[url.find('/') + 2:]
+
+
+"""
+Для проверки:
+
+assert domain_name("http://google.com") == "google"
+assert domain_name("http://google.co.jp") == "google"
+assert domain_name("www.xakep.ru") == "xakep"
+assert domain_name("https://youtube.com") == "youtube"
+"""
---
+++ zeros.py
@@ -0,0 +1,36 @@
+'''
+Написать метод zeros, который принимает на вход целое число (integer) и
+возвращает количество конечных нулей в факториале (N! = 1 * 2 * 3 * ... * N) заданного числа:
+Будьте осторожны 1000! имеет 2568 цифр.
+Доп. инфо: http://mathworld.wolfram.com/Factorial.html
+zeros(6) = 1
+# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+zeros(12) = 2
+# 12! = 479001600 --> 2 trailing zeros
+'''
+
+
+# Простое решение для прохождения проверки. 4 степень достаточна для !1000
+def zeros_simple(n: int):
+    return n // 5 + n // 5 ** 2 + n // 5 ** 3 + n // 5 ** 4
+
+
+# Полноценное решение для любого факториала
+def zeros(n: int):
+    summa, count, i = 0, None, 1
+
+    while count != 0:
+        count = n // 5 ** i
+        summa += count
+        i += 1
+    return summa
+
+
+"""
+Для проверки:
+
+assert zeros(0) == 0
+assert zeros(6) == 1
+assert zeros(30) == 7
+
+"""
